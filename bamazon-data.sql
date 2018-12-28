0a13ed0aa8a73ce11fc64d6764bc775fec999011
Emily Dalton <emilydalton@Emilys-MacBook-Air.local>
2018-12-28 09:08:43 -0600

    initial commits schema and data

---
+++ bamazon-data.sql
@@ -0,0 +1,18 @@
+-- Dumping data for products table
+--
+
+SET AUTOCOMMIT=0;
+INSERT INTO products VALUES (1,'Salad Shooter','Kitchen & Appliances',50, 9.99),
+(1,'Salad Shooter','Kitchen & Appliances',50, 9.99),
+(2,'HUGE TV','Electronics',100, 999.99),
+(3,'Really colorful plates','Kitchen & Appliances',40, 34.99),
+(4,'Moon Shoes','Footwear',10, 49.99),
+(5,'Candle shaped like a dog','Home Decor',200, 4.99),
+(6,'Dolphin print fleece blanket','Home Decor', 100, 9.99),
+(7,'1989 PGA Tour commemorative mug ','Kitchen & Appliances',400, 1.99),
+(8,'Eyes Wide Shut on Blue Ray','Electronics',25, 4.99),
+(9,'Mr. Met backpack NY Mets Backpack','Kids',30, 14.99),
+(10,'Scorpio Woman T-Shirt','Womeanswear',500, 4.99),
+
+
+COMMIT;
