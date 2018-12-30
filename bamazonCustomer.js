d4fe70c3bfb66900d56895dd2b19f6d66d0dfefc
Emily Dalton <emilydalton@Emilys-MacBook-Air.local>
2018-12-29 20:49:30 -0600

    adding list of items to bid on, first prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,47 +17,57 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
 });
+
+var displayitem = () =>{
+    const query = "SELECT * FROM products";
+    connection.query(query,function(err, results){
+       //  console.log(res);
+       
+       for(let i=0; i<results.length; i++){
+           const products = results[i]; 
+       //     console.log(products.product_name);
+       console.log(`Product ID: ${products.item_id} | Product Name: ${products.product_name} | Price (USD) ${products.price} `);
+         
+    }
+
+})
+}
 //first display all of the items available for sale. Include the ids, names, and prices of products for sale.
 var buyitem = () =>{
     
      // query the database for all items being auctioned
 const query = "SELECT * FROM products";
-     connection.query(query,function(err, res){
-         console.log(res);
+     connection.query(query,function(err, results){
+        //  console.log(res);
         
-        for(let i=0; i<res.length; i++){
-            const products = res[i]; 
-            console.log(products.product_name);
-        // for (var i = 0; i < results.length; i++) {
-        //     var item_id = results[i].item_id,
-        //     product_name = results[i].product_name,
-
-        //       department_name = results[i].department_name,
-        //       price = results[i].price,
-        //        stock_quantity = results[i].stock_quantity;
+        // for(let i=0; i<res.length; i++){
+        //     const products = res[i]; 
+        //     console.log(products.product_name);
+     
 
   
   
 
-        }  
-});
-}  
+  
         // once you have the items, prompt the user for which they'd like to bid on
-    //     inquirer
-    //       .prompt([
-    //         {
-    //           name: 'choice',
-    //           type: 'rawlist',
-    //           choices: function() {
-    //             var choiceArray = [];
-    //             for (var i = 0; i < results.length; i++) {
-    //               choiceArray.push(results[i].item_name);
-    //             }
-    //             return choiceArray;
-    //           },
-    //           message: "What item would you like to bid on?"
-    //         },
-    //       ])
-    //     });
-    // }
+        inquirer
+          .prompt([
+            {
+              name: 'choice',
+              type: 'rawlist',
+              choices: function() {
+                var choiceArray = [];
+                for (var i = 0; i < results.length; i++) {
+                const products = results[i];
+                  choiceArray.push(products.product_name);
+                }
+                return choiceArray;
+              },
+              message: "What item would you like to bid on?"
+            },
+          ])
+        
+        })
+    }
+    displayitem();
     buyitem();
