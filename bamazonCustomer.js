9ae167cfa017caa348871f08f3e56c0157559108
Emily Dalton <emilydalton@Emilys-MacBook-Air.local>
2018-12-29 21:55:01 -0600

    adding prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('easy-table')
+
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -19,15 +21,26 @@ connection.connect(function(err) {
 });
 
 var displayitem = () =>{
-    const query = "SELECT * FROM products";
+    const query = "SELECT item_id, product_name, price FROM products";
     connection.query(query,function(err, results){
        //  console.log(res);
        
        for(let i=0; i<results.length; i++){
            const products = results[i]; 
-       //     console.log(products.product_name);
-       console.log(`Product ID: ${products.item_id} | Product Name: ${products.product_name} | Price (USD) ${products.price} `);
          
+           console.log(Table.print(products));
+       //     console.log(products.product_name);
+    //    console.log(`Product ID: ${products.item_id} | Product Name: ${products.product_name} | Price (USD) ${products.price} `);
+    //    var t = new Table
+ 
+    //    forEach(function(products) {
+    //      t.cell('Product Id', products.item_id)
+    //      t.cell('Description', products.product_name)
+    //      t.cell('Price, USD',products.price, Table.number(2))
+    //      t.newRow()
+    //    })
+        
+    //    console.log(t.toString())
     }
 
 })
@@ -63,11 +76,17 @@ const query = "SELECT * FROM products";
                 }
                 return choiceArray;
               },
-              message: "What item would you like to bid on?"
+              message: "What item would you like to buy?"
             },
-          ])
-        
-        })
-    }
+            {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to buy?"
+          }
+        ])
+          
+    });
+}
+
     displayitem();
     buyitem();
